3975dd42463b8ba575d6ec76dd28dbf2dc5d1895
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-20 22:55:05 +0200

    Add floating action button

--- app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -53,6 +57,11 @@ class MainActivity : ComponentActivity() {
                                 }
 
                     },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { /*TODO*/ }) {
+                           Icon(Icons.Default.Add, contentDescription = "Add")
+                        }
+                    }
                 ) { innerPadding ->
                     Column(
                         modifier = Modifier.padding(innerPadding),
