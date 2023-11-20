f38ab1a45c47ff34c2cea0261658f13345fc46f2
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-20 22:53:35 +0200

    Add top and botton app bar

--- app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.zorrokid.mybasicjetpackcomposeapp.ui.theme.MyBasicJetpackComposeAppTheme
 
@@ -23,6 +29,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyBasicJetpackComposeAppTheme {
                 Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("Top app bar")
+                            }
+                        )
+                    },
+                    bottomBar = {
+                                BottomAppBar(
+                                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                    contentColor = MaterialTheme.colorScheme.primary,
+                                ) {
+                                    Text(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center,
+                                        text = "Bottom app bar"
+                                    )
+                                }
+
+                    },
                 ) { innerPadding ->
                     Column(
                         modifier = Modifier.padding(innerPadding),
