31596efd1c3d1a4fa7691a313d8c30f65b2cc166
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-20 23:06:22 +0200

    Separate Scaffold to new file

--- app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/MainActivity.kt
@@ -3,105 +3,13 @@ package com.zorrokid.mybasicjetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import com.zorrokid.mybasicjetpackcomposeapp.ui.theme.MyBasicJetpackComposeAppTheme
-
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyBasicJetpackComposeAppTheme {
-
-                var presses by remember { mutableStateOf(0) }
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            colors = TopAppBarDefaults.mediumTopAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                            title = {
-                                Text("Top app bar")
-                            }
-                        )
-                    },
-                    bottomBar = {
-                                BottomAppBar(
-                                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                    contentColor = MaterialTheme.colorScheme.primary,
-                                ) {
-                                    Text(
-                                        modifier = Modifier.fillMaxWidth(),
-                                        textAlign = TextAlign.Center,
-                                        text = "Bottom app bar"
-                                    )
-                                }
-
-                    },
-                    floatingActionButton = {
-                        FloatingActionButton(onClick = { presses++ }) {
-                           Icon(Icons.Default.Add, contentDescription = "Add")
-                        }
-                    }
-                ) { innerPadding ->
-                    Column(
-                        modifier = Modifier.padding(innerPadding),
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                    ) {
-                        Greeting("Scaffold")
-                        CounterText(presses)
-                    }
-                }
-            }
+            MyBasicJetpackComposeApp()
         }
     }
 }
 
-@Composable
-fun CounterText(presses: Int, modifier: Modifier = Modifier) {
-    Text(
-        text = "You've pressed $presses times.",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyBasicJetpackComposeAppTheme {
-        Greeting("Android")
-    }
-}
