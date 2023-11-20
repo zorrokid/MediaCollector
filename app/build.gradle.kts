44c736571d9a72228e48bfd3a000769a1696edd0
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-20 22:11:38 +0200

    Use compileSdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.zorrokid.mybasicjetpackcomposeapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.zorrokid.mybasicjetpackcomposeapp"
