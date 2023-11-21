3e0db1bf219a1e442831cffb7ed80d2bc7fc39db
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-21 23:42:42 +0200

    Add email and password fields and visibility icons

--- app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/screens/login/LoginScreen.kt
+++ app/src/main/java/com/zorrokid/mybasicjetpackcomposeapp/screens/login/LoginScreen.kt
@@ -1,14 +1,104 @@
 package com.zorrokid.mybasicjetpackcomposeapp.screens.login
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.zorrokid.mybasicjetpackcomposeapp.R
 
 @Composable
-fun LogInScreen() {
+fun LogInScreen(){
     LogInScreenContent()
 }
 
 @Composable
-fun LogInScreenContent() {
-    Text("LogInScreenContent")
+fun LogInScreenContent(modifier: Modifier = Modifier,) {
+    Column(
+        modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        EmailField("email", onNewValue = { /*TODO*/ })
+        PasswordField(value = "", onNewValue = { /* TODO */})
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailField(value: String, onNewValue: (String) -> Unit, modifier: Modifier = Modifier) {
+    OutlinedTextField(
+        singleLine = true,
+        modifier = modifier,
+        value = value,
+        onValueChange = { onNewValue(it) },
+        placeholder = { Text(stringResource(id = R.string.email)) },
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = stringResource(
+            id = R.string.email    
+        )) }
+    )
+}
+
+@Composable
+fun PasswordField(value: String, onNewValue: (String) -> Unit, modifier: Modifier = Modifier) {
+    PasswordField(value, R.string.password, onNewValue, modifier)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun PasswordField(
+    value: String,
+    @StringRes placeholder: Int,
+    onNewValue: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var isVisible by remember { mutableStateOf(false) }
+
+    val icon =
+        if (isVisible) painterResource(R.drawable.visibility_24px)
+        else painterResource(R.drawable.visibility_off_24px)
+
+    val visualTransformation =
+        if (isVisible) VisualTransformation.None else PasswordVisualTransformation()
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = { onNewValue(it) },
+        placeholder = { Text(text = stringResource(placeholder)) },
+        leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock") },
+        trailingIcon = {
+            IconButton(onClick = { isVisible = !isVisible }) {
+                Icon(painter = icon, contentDescription = "Visibility")
+            }
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = visualTransformation
+    )
 }
