e0c95f06230f0401edc6429d96ff0dae055e9101
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-12-10 18:41:44 +0200

    Add camera preview

--- app/src/main/java/com/zorrokid/mediacollector/screens/text_recognition/TextRecognitionScreen.kt
+++ app/src/main/java/com/zorrokid/mediacollector/screens/text_recognition/TextRecognitionScreen.kt
@@ -1,16 +1,26 @@
 package com.zorrokid.mediacollector.screens.text_recognition
 
 import android.Manifest
+import android.widget.LinearLayout
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
@@ -36,9 +46,7 @@ fun TextRecognitionScreenContent(
     val openPermissionDialog = remember { mutableStateOf(false) }
 
     if (cameraPermissionState.status.isGranted) {
-        Button(onClick = {}) {
-            Text("Scan")
-        }
+        CameraPreview()
     }
     else {
         Column {
@@ -70,4 +78,41 @@ fun TextRecognitionScreenContent(
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CameraPreview(modifier: Modifier = Modifier){
+
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val cameraController = remember {
+        LifecycleCameraController(context)
+    }
+
+    Scaffold(
+        content = { padding ->
+            Column(
+                modifier = modifier
+                .fillMaxSize()
+                .padding(padding)
+            ) {
+                AndroidView(
+                    modifier = modifier.fillMaxSize().padding(padding),
+                    factory = { context ->
+                    PreviewView(context).apply {
+                        layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT
+                        )
+                        scaleType = PreviewView.ScaleType.FILL_START
+                    }.also { previewView ->
+                        previewView.controller = cameraController
+                        cameraController.bindToLifecycle(lifecycleOwner)
+                    }
+                })
+            }
+        }
+    )
+
 }
