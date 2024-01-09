824b64ea8b4ddd27aedc1dbc7ac7f87dfc01912a
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-01-09 23:42:19 +0200

    Add Bottom sheet modal to show matching results with copy button

--- app/src/main/java/com/zorrokid/mediacollector/screens/add_or_edit_item/AddOrEditItemUiState.kt
+++ app/src/main/java/com/zorrokid/mediacollector/screens/add_or_edit_item/AddOrEditItemUiState.kt
@@ -1,9 +1,12 @@
 package com.zorrokid.mediacollector.screens.add_or_edit_item
 
+import com.zorrokid.mediacollector.model.CollectionItem
+
 data class AddOrEditItemUiState(
     val id: String = "",
     val name: String = "",
     val barcode: String = "",
     val releaseAreaId: String = "",
     val conditionClassificationId: String = "",
+    val searchResults: List<CollectionItem> = emptyList(),
 )
